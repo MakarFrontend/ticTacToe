8b08ebad748411d12b2f2073dae092ba456f5260
Makar <makar_zag@mail.ru>
2024-05-23 12:11:34 +0300

    Написал тесты для CheckJSON

--- checkLevel/check_test.go
+++ checkLevel/check_test.go
@@ -3,5 +3,18 @@ package check
 import "testing"
 
 func TestCheckJSON(t *testing.T) {
-
+	var optinos map[string]bool = map[string]bool{
+		"test-false.json":  false,
+		"test-true.json":   true,
+		"test2-false.json": false,
+		"test2-true.json":  true,
+	}
+	for key, val := range optinos {
+		t.Run(key, func(t *testing.T) {
+			res := CheckJSON(key)
+			if res != val {
+				t.Errorf("Ошибка в %v", key)
+			}
+		})
+	}
 }
