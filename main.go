3c457ccdee329d0f1603a8ba5f26b7c6097545a7
Makar <makar_zag@mail.ru>
2024-05-23 19:09:00 +0300

    Финальная версия игры

--- main.go
+++ main.go
@@ -2,9 +2,12 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	check "krestNulls/checkLevel"
 	"krestNulls/game" //Пакет с игровыми функциями
 	"os"
+	"strconv"
 )
 
 func main() {
@@ -14,18 +17,21 @@ func main() {
 		nameOfMap string       //Имя для игровой карты
 		gameType  string       //Тип действия
 	)
+	sc := bufio.NewScanner(os.Stdin)                   //Сканер из стандартного ввода
 	fmt.Println("ДОБРО ПОЖАЛОВАТЬ В КРЕСТИКИ НОЛИКИ!") //Приветствие
 
 	for { //Начало бесконечного цикла
 		for { //Ждём адекватный уровень
 			fmt.Print("Уровень: ") //Какой уровень
-			fmt.Scan(&nameOfMap)
+			sc.Scan()
+			nameOfMap = sc.Text()
 			if nameOfMap == "f" { //Если f, то уровень с компьютера
 				var (
 					f    string //Имя уровня
 					errr error  //Ошибка
 				)
-				fmt.Scan(&f)
+				sc.Scan()
+				f = sc.Text()
 				gameMap, errr = game.GetUserLevel(f + ".json") //Берём пользовательский уровень
 				if errr != nil {
 					fmt.Println(errr)
@@ -34,6 +40,9 @@ func main() {
 					gameType = "game"
 					break
 				}
+			} else if nameOfMap == "check" { //Для проверки корректности уровня
+				gameType = "check"
+				break
 			} else {
 				_, ok := game.Levels[nameOfMap]
 				if ok {
@@ -41,27 +50,34 @@ func main() {
 					for i := range gameMap.GameMap { //Заполняем карту нулями
 						gameMap.GameMap[i] = 0
 					}
-					gameType = "game"
+					gameType = "game" //Устанавливем тип игры как "game"
 					break
 				} else {
 					fmt.Printf("Уровня %v ещё нет((\n", nameOfMap)
 				}
 			}
 		}
+
 		switch gameType { //Какой тип игры
 		case "game": //Если это игра
-			var i int = 1
-			for { //Бесконечный цикл для ставки ноликов и крестиков
+			var i int = 1 //На случай если компьютер первый
+			for {         //Бесконечный цикл для ставки ноликов и крестиков
 				if gameMap.First && i == 1 {
 					gameMap.ComputerDoShoot()
-					i--
+					i-- //Чтобы больше компьютер не ходил в начале хода
 				}
 				fmt.Println("Твой ход!")
 				fmt.Println(gameMap.PrintMap()) //Печатаем карту первый раз
 				for {                           //Делаем бесконечный цикл и ждём пока пользователь не введёт что-то адекватное
 					fmt.Print("Введи куда ставить нолик: ")
-					fmt.Scan(&shoot)
-					if shoot == -1 { //Прерываем цикл если хотим выйти из партии
+					sc.Scan()
+					shoot64, er := strconv.ParseInt(sc.Text(), 10, 64)
+					if er == nil {
+						shoot = int(shoot64)
+					}
+					if er != nil {
+						fmt.Println("Неправильная координата стрельбы")
+					} else if shoot == -1 { //Прерываем цикл если хотим выйти из партии
 						break
 					} else if shoot == -2 { //-2 - свайп хода
 						fmt.Fprintln(os.Stdout, "Ход пропущен")
@@ -102,6 +118,17 @@ func main() {
 					}
 				}
 			}
+		case "check": //Проверка корректности уровня
+			fmt.Print("Введи название уровня для проверки: ")
+			sc.Scan()
+			name := sc.Text()
+			res := check.CheckJSON(name + ".json")
+			if res {
+				fmt.Printf("Уровень %v корректный\n", name)
+			} else {
+				fmt.Printf("Уровень %v не корректный\n", name)
+			}
 		}
+
 	}
 }
